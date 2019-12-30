dbc8426695d2645be195f634aae01a77e8db8aba
Natasha <nangl003@fiu.edu>
2019-12-29 19:04:26 -0500

    Completed TechJobs Object-Oriented Assignment

--- src/TechJobs/ViewModels/NewJobViewModel.cs
+++ src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -15,6 +15,19 @@ namespace TechJobs.ViewModels
         [Display(Name = "Employer")]
         public int EmployerID { get; set; }
 
+        [Required]
+        [Display(Name = "Location")]
+        public int Location { get; set; }
+
+        [Required]
+        [Display(Name = "CoreCompetency")]
+        public int CoreCompetency { get; set; }
+
+        [Required]
+        [Display(Name = "PositionType")]
+        public int PositionType { get; set; }
+
+
         // TODO #3 - Included other fields needed to create a job,
         // with correct validation attributes and display names.
 
@@ -39,6 +52,32 @@ namespace TechJobs.ViewModels
             // TODO #4 - populate the other List<SelectListItem> 
             // collections needed in the view
 
+            foreach (Location field in jobData.Locations.ToList())
+            {
+                Locations.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (CoreCompetency field in jobData.CoreCompetencies.ToList())
+            {
+                CoreCompetencies.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (PositionType field in jobData.PositionTypes.ToList())
+            {
+                PositionTypes.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
         }
     }
 }
